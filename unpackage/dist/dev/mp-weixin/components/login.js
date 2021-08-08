87f608ec2da0c8446de8e92c805cf7bca4b2180c
Azure <965787894@qq.com>
2021-08-08 23:35:41 +0800

    登录组件二次更新，新增验证输入等功能

--- unpackage/dist/dev/mp-weixin/components/login.js
+++ unpackage/dist/dev/mp-weixin/components/login.js
@@ -81,6 +81,21 @@ try {
   components = {
     uIcon: function() {
       return __webpack_require__.e(/*! import() | node-modules/uview-ui/components/u-icon/u-icon */ "node-modules/uview-ui/components/u-icon/u-icon").then(__webpack_require__.bind(null, /*! uview-ui/components/u-icon/u-icon.vue */ 98))
+    },
+    uForm: function() {
+      return __webpack_require__.e(/*! import() | node-modules/uview-ui/components/u-form/u-form */ "node-modules/uview-ui/components/u-form/u-form").then(__webpack_require__.bind(null, /*! uview-ui/components/u-form/u-form.vue */ 114))
+    },
+    uFormItem: function() {
+      return Promise.all(/*! import() | node-modules/uview-ui/components/u-form-item/u-form-item */[__webpack_require__.e("common/vendor"), __webpack_require__.e("node-modules/uview-ui/components/u-form-item/u-form-item")]).then(__webpack_require__.bind(null, /*! uview-ui/components/u-form-item/u-form-item.vue */ 121))
+    },
+    uInput: function() {
+      return Promise.all(/*! import() | node-modules/uview-ui/components/u-input/u-input */[__webpack_require__.e("common/vendor"), __webpack_require__.e("node-modules/uview-ui/components/u-input/u-input")]).then(__webpack_require__.bind(null, /*! uview-ui/components/u-input/u-input.vue */ 132))
+    },
+    uButton: function() {
+      return __webpack_require__.e(/*! import() | node-modules/uview-ui/components/u-button/u-button */ "node-modules/uview-ui/components/u-button/u-button").then(__webpack_require__.bind(null, /*! uview-ui/components/u-button/u-button.vue */ 139))
+    },
+    uSubsection: function() {
+      return __webpack_require__.e(/*! import() | node-modules/uview-ui/components/u-subsection/u-subsection */ "node-modules/uview-ui/components/u-subsection/u-subsection").then(__webpack_require__.bind(null, /*! uview-ui/components/u-subsection/u-subsection.vue */ 146))
     }
   }
 } catch (e) {
@@ -137,7 +152,45 @@ __webpack_require__.r(__webpack_exports__);
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-/* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0; //
+/* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _regenerator = _interopRequireDefault(__webpack_require__(/*! ./node_modules/@babel/runtime/regenerator */ 19));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {try {var info = gen[key](arg);var value = info.value;} catch (error) {reject(error);return;}if (info.done) {resolve(value);} else {Promise.resolve(value).then(_next, _throw);}}function _asyncToGenerator(fn) {return function () {var self = this,args = arguments;return new Promise(function (resolve, reject) {var gen = fn.apply(self, args);function _next(value) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);}function _throw(err) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);}_next(undefined);});};} //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 //
 //
 //
@@ -161,19 +214,167 @@ __webpack_require__.r(__webpack_exports__);
 //
 var _default =
 {
-  data: function data() {
+  data: function data() {var _this = this;
     return {
       show: true,
-      getUserInfoTag: true };
+      getUserInfoTag: true,
+      // 登陆方式
+      loginType: 'pwlogin',
+      // 登陆方式选择器
+      subsectionList: [{
+        name: '密码登陆' },
+
+      {
+        name: '验证码登陆' },
+
+      {
+        name: '手机注册' }],
+
+
+      // 用户信息输入框
+      form: {
+        login: '',
+        avatar: '',
+        name: '',
+        phone: '',
+        code: '',
+        password: '',
+        repassword: '' },
+
+      // 用户登录校验规则
+      rules: {
+        // 密码登陆账号校验，该输入可以是：手机号、昵称
+        login: [{
+          validator: function validator(rule, value, callback) {
+            // 只有在 密码登陆 的情况下才需要进行前端规则校验
+            if (_this.loginType === 'pwlogin') {
+              return !_this.$u.test.isEmpty(value);
+            } else {
+              return true;
+            }
+          },
+          message: '必填 * 输入内容不许为空',
+          trigger: ['change', 'blur'] }],
+
+        // 电话、邮件注册校验
+        name: [{
+          asyncValidator: function () {var _asyncValidator = _asyncToGenerator( /*#__PURE__*/_regenerator.default.mark(function _callee(rule, value, callback) {var res;return _regenerator.default.wrap(function _callee$(_context) {while (1) {switch (_context.prev = _context.next) {case 0:if (!(
+
+                      _this.loginType === 'phone' || _this.loginType === 'email')) {_context.next = 7;break;}_context.next = 3;return (
+                        _this.$u.api.findUser({
+                          name: value }));case 3:res = _context.sent;
+
+                      if (!!value && res.statusCode === 200) {
+                        callback(new Error('当前用户昵称已存在'));
+                      } else {
+                        callback();
+                      }_context.next = 8;break;case 7:
+
+                      callback();case 8:case "end":return _context.stop();}}}, _callee);}));function asyncValidator(_x, _x2, _x3) {return _asyncValidator.apply(this, arguments);}return asyncValidator;}(),
+
+
+          trigger: ['blur'] },
+
+        {
+          validator: function validator(rule, value, callback) {
+            if (_this.loginType === 'phone' || _this.loginType === 'email') {
+              return _this.$u.test.rangeLength(value, [2, 10]);
+            } else {
+              return true;
+            }
+          },
+          message: '必填 * 昵称长度不得小于2位大于10位',
+          trigger: ['change', 'blur'] }],
+
+
+        // 手机验证码登陆、手机验证码注册 使用
+        phone: [{
+          asyncValidator: function () {var _asyncValidator2 = _asyncToGenerator( /*#__PURE__*/_regenerator.default.mark(function _callee2(rule, value, callback) {var res;return _regenerator.default.wrap(function _callee2$(_context2) {while (1) {switch (_context2.prev = _context2.next) {case 0:if (!(
+
+                      _this.loginType === 'phone')) {_context2.next = 7;break;}_context2.next = 3;return (
+                        _this.$u.api.findUser({
+                          name: value }));case 3:res = _context2.sent;
+
+                      if (!!value && res.statusCode === 200) {
+                        callback(new Error('当前电话已注册'));
+                      } else {
+                        // 如果校验通过，也要执行callback()回调
+                        callback();
+                      }_context2.next = 8;break;case 7:
+
+                      callback();case 8:case "end":return _context2.stop();}}}, _callee2);}));function asyncValidator(_x4, _x5, _x6) {return _asyncValidator2.apply(this, arguments);}return asyncValidator;}(),
+
+
+          trigger: ['blur'] },
+
+        {
+          validator: function validator(rule, value, callback) {
+            // 手机号码格式判断
+            if (_this.loginType === 'codelogin' || _this.loginType === 'phone') {
+              // 自带判断 电话号码合法性方法
+              return _this.$u.test.mobile(value);
+            } else {
+              return true;
+            }
+          },
+          message: '必填 * 手机号码输入不正确',
+          trigger: ['change', 'blur'] }],
+
+
+        // 验证码验证
+        code: [{
+          validator: function validator(rule, value, callback) {
+            if (_this.loginType !== 'pwlogin') {
+              // 名字长度为 2-10 位
+              return value === '8888';
+            } else {
+              return true;
+            }
+          },
+          message: '必填 * 验证码长度为4位',
+          // 触发器可以同时用blur和change
+          trigger: ['change', 'blur'] }],
+
+        // 密码验证
+        password: [{
+          validator: function validator(rule, value, callback) {
+            // 除了 手机验证码登陆 其他均需要验证
+            if (_this.loginType !== 'codelogin') {
+              // 名字长度为 2-10 位
+              return value.length >= 4 && value.length <= 20;
+            } else {
+              return true;
+            }
+          },
+          message: '必填  * 请输入 4-20 位密码',
+          trigger: ['change', 'blur'] }],
+
+        repassword: [{
+          validator: function validator(rule, value, callback) {
+            if (_this.loginType === 'phone' || _this.loginType === 'email') {
+              return _this.form.password === value;
+            } else {
+              return false;
+            }
+
+          },
+          message: '必填 * 两次密码输入不一致',
+          // 触发器可以同时用blur和change
+          trigger: ['change', 'blur'] }] } };
+
+
 
   },
-  created: function created() {var _this = this;
+  created: function created() {var _this2 = this;
     wx.getSetting({
       success: function success(res) {
         if (res.authSetting["scope.userInfo"]) {
           uni.getUserInfo({
             success: function success(res) {
-              _this.getUserInfoTag = false;
+              _this2.getUserInfoTag = false;
+              _this2.form.login = res.userInfo.nickName;
+              _this2.form.name = res.userInfo.nickName;
+              _this2.form.avatar = res.userInfo.avatarUrl;
             },
             fail: function fail() {
               console.log('用户未授权！');
@@ -182,6 +383,10 @@ var _default =
         }
       } });
 
+  },
+  // 应用校验规则
+  onReady: function onReady() {
+    this.$refs.uForm.setRules(this.rules);
   },
   methods: {
     // 关闭弹窗
@@ -198,14 +403,132 @@ var _default =
 
     },
     // 微信授权
-    getWechatUserInfo: function getWechatUserInfo() {var _this2 = this;
+    getWechatUserInfo: function getWechatUserInfo() {var _this3 = this;
       uni.getUserInfo({
         success: function success(res) {
-          _this2.getUserInfoTag = false;
+          _this3.getUserInfoTag = false;
+          _this3.form.login = res.userInfo.nickName;
+          _this3.form.name = res.userInfo.nickName;
+          _this3.form.avatar = res.userInfo.avatarUrl;
         },
         fail: function fail() {
           console.log('用户未授权！');
         } });
+
+    },
+    // 获取验证码
+    getCode: function getCode() {var _this4 = this;return _asyncToGenerator( /*#__PURE__*/_regenerator.default.mark(function _callee3() {var pcode;return _regenerator.default.wrap(function _callee3$(_context3) {while (1) {switch (_context3.prev = _context3.next) {case 0:_context3.next = 2;return (
+                  _this4.$u.api.getLoginCode({
+                    phone: _this4.form.phone }));case 2:pcode = _context3.sent;
+
+                if (!!pcode.data.message && pcode.data.message[0] === "获取成功") {
+                  uni.showModal({
+                    title: '验证码获取成功',
+                    content: '8888' });
+
+                } else {
+                  uni.showModal({
+                    title: '验证码获取失败',
+                    content: pcode.data.errors.phone[0] });
+
+                }case 4:case "end":return _context3.stop();}}}, _callee3);}))();
+    },
+    submit: function submit() {var _this5 = this;
+      this.$refs.uForm.validate( /*#__PURE__*/function () {var _ref = _asyncToGenerator( /*#__PURE__*/_regenerator.default.mark(function _callee4(valid) {var resa, resaa, resb;return _regenerator.default.wrap(function _callee4$(_context4) {while (1) {switch (_context4.prev = _context4.next) {case 0:if (
+                  valid) {_context4.next = 3;break;}
+                  uni.showToast({
+                    title: '输入格式有误，请重新检查',
+                    icon: 'none' });return _context4.abrupt("return",
+
+                  false);case 3:_context4.t0 =
+
+                  _this5.loginType;_context4.next = _context4.t0 ===
+                  "pwlogin" ? 6 : _context4.t0 ===
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  "codelogin" ? 11 : _context4.t0 ===
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  "phone" ? 16 : 21;break;case 6:_context4.next = 8;return _this5.$u.api.userLogin({ login: _this5.form.login, password: _this5.form.password });case 8:resa = _context4.sent;if (resa.statusCode === 200) {// 登陆成功
+                    _this5.loginAfter(resa.data.access_token);} else {uni.showModal({ title: '登陆失败', content: resa.data.message });}return _context4.abrupt("break", 23);case 11:_context4.next = 13;return _this5.$u.api.userLogin({ login: _this5.form.phone, verifiable_code: '8888' });case 13:resaa = _context4.sent;if (resaa.statusCode === 200) {// 登陆成功
+                    _this5.loginAfter(resaa.data.access_token);} else {uni.showModal({ title: '登陆失败', content: resaa.data.message });}return _context4.abrupt("break", 23);case 16:_context4.next = 18;return _this5.$u.api.userRegister({ // 必须，用户名
+                    name: _this5.form.name,
+                    // 必须，验证码发送模式。
+                    verifiable_type: 'sms',
+                    // 必须，用户收到的验证码。
+                    verifiable_code: '8888',
+                    // 如果 `verifiable_type` 为 `sms` 则必须, 手机号码。
+                    phone: _this5.form.phone,
+                    // 可选，密码，如果不输入密码，允许用户无密码注册。
+                    password: _this5.form.password });case 18:resb = _context4.sent;
+
+                  if (resb.statusCode === 201) {
+                    // 登陆成功
+                    _this5.loginAfter(resb.data.token);
+                  } else {
+                    uni.showModal({
+                      title: '登陆失败',
+                      content: resb.data.message });
+
+                  }return _context4.abrupt("break", 23);case 21:
+
+
+                  uni.showToast({
+                    title: '未知用户状态',
+                    icon: 'loading' });return _context4.abrupt("break", 23);case 23:case "end":return _context4.stop();}}}, _callee4);}));return function (_x7) {return _ref.apply(this, arguments);};}());
+
+
+
+
+
+    },
+    // 取消表单输入
+    cancel: function cancel() {
+      this.form = {
+        login: '',
+        name: '',
+        phone: '',
+        code: '',
+        password: '',
+        repassword: '' };
+
+    },
+    // 更改 登陆 注册 方式选择
+    sectionChange: function sectionChange(index) {
+      switch (index) {
+        case 1:
+          this.loginType = "codelogin";
+          break;
+        case 2:
+          this.loginType = "phone";
+          break;
+        default:
+          this.loginType = "pwlogin";
+          break;}
 
     } } };exports.default = _default;
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["default"]))
